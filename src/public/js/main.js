f8f2c83873e9418a0189c5ff26c16de83665e5fc
YeinerMolina <yeinerm@uninorte.edu.co>
2022-03-23 10:11:43 -0500

    add polyline

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -2,17 +2,18 @@
 var map = L.map('map-template');
 TileURL = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
 L.tileLayer(TileURL).addTo(map);
+PolyArray = [];
 
+var MapIcon = L.Icon.extend({
+    options: {
+        iconSize: [40,40],
+        iconAnchor: [22,94],
+        popupAnchor: [-3,-76]
+    }
+})
 
 //Sockets
 const socket  = io();
-
-socket.on('server: NewUserCoordenates',(data) =>{
-    console.log('New Data Recived')
-    console.log(data)
-    Actualizar(data);
-    UpdateMap(data);
-})
 
 // Usamos selector para asignar el valor de la fila en un id
 id = document.getElementById('id')
@@ -44,17 +45,25 @@ function Actualizar(data){
 
 
 //Actualizar la posición en el mapa
+
 function UpdateMap(data){
     Lat = data[0].latitud;
     Lon = data[0].longitud;
+    PolyArray.push([Lat,Lon]);
     if (typeof marker !== 'undefined'){
         map.removeLayer(marker);
     }else{
         map.setView([Lat,Lon],14);
     }
+    (typeof PolyLine !== 'undefined'){
+        map.removeLayer(PolyLine);
+    }
+
     map.setView([Lat,Lon]);
     marker = L.marker([Lat,Lon]);
+    PolyLine = L.polyline(PolyArray,{color:'red'})
     marker.bindPopup("Latitud: " + Lat + ", Longitud: "+ Lon);
     map.addLayer(marker);
+    map.addLayer(PolyLine);
 }
 
