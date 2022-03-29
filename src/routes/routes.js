a3508f0d4f3a8d775db01f40e6407931b600a0e1
MarceMatos <101466345+MarceMatos@users.noreply.github.com>
2022-03-29 17:32:31 -0500

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -20,7 +20,7 @@ router.get('/getData',(req,res) =>{
 })
 //El de la mitad obtiene una lista de los programas que ocupan el puerto 3000. 
 router.post('/github',(req,res)=>{
-    exec('git pull origin main && sudo kill -9 $(sudo lsof -t -i:3000) && npm start')
+    exec('git pull && sudo kill -9 $(sudo lsof -t -i:3000) && npm start')
     
 })
 const connection = require('../../database/db.js');
