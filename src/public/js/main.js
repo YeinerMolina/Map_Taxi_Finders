046d30962a9dd23969ca2a7a8118c3fdc4676a62
MarceMatos <101466345+MarceMatos@users.noreply.github.com>
2022-03-28 22:02:46 -0500

    Update main.js

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,4 +1,71 @@
+//Crear el mapa
+var map = L.map('map-template');
+TileURL = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
+L.tileLayer(TileURL).addTo(map);
+PolyArray = [];
 
+var MapIcon = L.Icon.extend({
+    options: {
+        iconSize: [40,40],
+        iconAnchor: [22,94],
+        popupAnchor: [-3,-76]
+    }
+})
+
+//Sockets
+const socket  = io();
+
+// Usamos selector para asignar el valor de la fila en un id
+id = document.getElementById('id')
+latitud = document.getElementById('latitud')
+longitud = document.getElementById('longitud')
+fecha = document.getElementById('fecha')
+hora = document.getElementById('hora')  
+
+// crea un long polling para simular un socket y pedir los datos periodicamente con un intervalos de 5seg
+setInterval(() => {
+    $.ajax({
+        url: '/getData',
+        success: function(data){
+            Actualizar(data);
+            UpdateMap(data);
+        }
+    })    
+
+}, 1000);
+
+//Actualizar la ubicación en la tarjeta
+function Actualizar(data){
+    id.innerHTML = data[0].ID             // innerHTML establece la conexion en los id's 
+    latitud.innerHTML = data[0].latitud
+    longitud.innerHTML = data[0].longitud
+    fecha.innerHTML = data[0].fecha.replace("T00:00:00.000Z","")
+    hora.innerHTML = data[0].hora
+}
+
+
+//Actualizar la posición en el mapa
+
+function UpdateMap(data){
+    Lat = data[0].latitud;
+    Lon = data[0].longitud;
+    PolyArray.push([Lat,Lon]);
+    if (typeof marker !== 'undefined'){
+        map.removeLayer(marker);
+    }else{
+        map.setView([Lat,Lon],14);
+    }
+    if (typeof PolyLine !== 'undefined'){
+        map.removeLayer(PolyLine);
+    }
+
+    map.setView([Lat,Lon]);
+    marker = L.marker([Lat,Lon]);
+    PolyLine = L.polyline(PolyArray,{color:'red'})
+    marker.bindPopup("Latitud: " + Lat + ", Longitud: "+ Lon);
+    map.addLayer(marker);
+    map.addLayer(PolyLine);
+}
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Make globaly available as well
@@ -1567,73 +1634,6 @@
     };
 
     return DateRangePicker;
-//Crear el mapa
-var map = L.map('map-template');
-TileURL = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
-L.tileLayer(TileURL).addTo(map);
-PolyArray = [];
 
-var MapIcon = L.Icon.extend({
-    options: {
-        iconSize: [40,40],
-        iconAnchor: [22,94],
-        popupAnchor: [-3,-76]
-    }
-})
-
-//Sockets
-const socket  = io();
-
-// Usamos selector para asignar el valor de la fila en un id
-id = document.getElementById('id')
-latitud = document.getElementById('latitud')
-longitud = document.getElementById('longitud')
-fecha = document.getElementById('fecha')
-hora = document.getElementById('hora')  
-
-// crea un long polling para simular un socket y pedir los datos periodicamente con un intervalos de 5seg
-setInterval(() => {
-    $.ajax({
-        url: '/getData',
-        success: function(data){
-            Actualizar(data);
-            UpdateMap(data);
-        }
-    })    
-
-}, 1000);
-
-//Actualizar la ubicación en la tarjeta
-function Actualizar(data){
-    id.innerHTML = data[0].ID             // innerHTML establece la conexion en los id's 
-    latitud.innerHTML = data[0].latitud
-    longitud.innerHTML = data[0].longitud
-    fecha.innerHTML = data[0].fecha.replace("T00:00:00.000Z","")
-    hora.innerHTML = data[0].hora
-}
-
-
-//Actualizar la posición en el mapa
-
-function UpdateMap(data){
-    Lat = data[0].latitud;
-    Lon = data[0].longitud;
-    PolyArray.push([Lat,Lon]);
-    if (typeof marker !== 'undefined'){
-        map.removeLayer(marker);
-    }else{
-        map.setView([Lat,Lon],14);
-    }
-    if (typeof PolyLine !== 'undefined'){
-        map.removeLayer(PolyLine);
-    }
-
-    map.setView([Lat,Lon]);
-    marker = L.marker([Lat,Lon]);
-    PolyLine = L.polyline(PolyArray,{color:'red'})
-    marker.bindPopup("Latitud: " + Lat + ", Longitud: "+ Lon);
-    map.addLayer(marker);
-    map.addLayer(PolyLine);
-}
 
 }));
