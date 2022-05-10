68ad829b8619cc0ac930ee921e81249aacf4a370
MarceMatos <101466345+MarceMatos@users.noreply.github.com>
2022-05-09 23:44:21 -0500

    reinicio, intento 2 cambiar color rojo

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -153,6 +153,14 @@ function Actualizar(data){
 
 }
 
+var greenIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
 
 //Actualice the marker position and poliline
 function UpdateMap(data){
@@ -178,15 +186,6 @@ function UpdateMap(data){
     if (typeof PolyLineT2 !== 'undefined'){
         map.removeLayer(PolyLineT2);
     }
-    var greenIcon = new L.Icon({
-      iconUrl: 'https://github.com/pointhi/leaflet-color-markers/blob/master/img/marker-icon-red.png',
-      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-      iconSize: [25, 41],
-      iconAnchor: [12, 41],
-      popupAnchor: [1, -34],
-      shadowSize: [41, 41]
-    });
-
     
     if (data[0].ID == 1){
         PolyArrayT1.push([Lat,Lon]);
@@ -200,7 +199,7 @@ function UpdateMap(data){
         
     }else if(data[0].ID == 2){
         PolyArrayT2.push([Lat,Lon]);
-        markerT2 = L.marker([Lat,Lon], {icon: greenIcon}).addTo(map);
+        markerT2 = L.marker([Lat,Lon],{icon: greenIcon});
         PolyLineT2 = L.polyline(PolyArrayT2,{color:'red'})
         markerT2.bindPopup("Posición actual taxi 2");
 
